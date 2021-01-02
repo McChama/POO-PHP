05a6195b090c0ec9093a5686eb477c22d31cc0f1
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-02 16:58:26 -0600

    The class now have parameters

---
+++ sistema/Conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+    class Conexion{
+        private $host = "localhost";
+        private $user = "root";
+        private $password = "";
+        private $db = "prueba";
+        private $connect;
+    }
+
+?>
