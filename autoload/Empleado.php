44cd8dc98c4d7e2980c27eb76b422051bb70b654
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-02 16:49:33 -0600

    Update autload

---
+++ autoload/Empleado.php
@@ -0,0 +1,40 @@
+<?php
+    //require_once("classPersona.php");
+    require_once("autoload.php");
+    class Empleado extends Persona{
+
+        protected $strPuesto;
+
+        function __construct(int $dpi, string $nombre, int $edad){
+            parent::__construct($dpi, $nombre, $edad);
+        }
+
+        public function setPuesto(string $puesto){
+            $this->strPuesto = $puesto;
+        }
+        
+        public function getPuesto():string{
+            return $this->strPuesto;
+        }
+
+        public function setMensaje(string $mensaje){
+            $this->mensaje = $mensaje;
+        }
+        
+        public function getMensaje():string{
+            return $this->mensaje . " " . $this->strNombre;
+        }
+
+        public function getDatosPersonales():string{
+            $datos = "
+                <h2>Datos personales</h2>
+                DPI: {$this->intDpi} <br>
+                Nombre: {$this->strNombre} <br>
+                Edad: {$this->intEdad} <br>
+            ";
+
+            return $datos;
+        }
+
+    }//End class Empleado
+?>
---
+++ autoload/empresa.php
@@ -0,0 +1,27 @@
+<?php
+    //require_once("classEmpleado.php");
+    //require_once("classCliente.php");
+    require_once("autoload.php");
+
+    $objEmpleado = new Empleado(78978, "Emiliano Vázquez Banda", 21);
+    $objEmpleado->setPuesto("Administrador");
+    $objEmpleado->setMensaje("Bienvenido empleado ");
+
+    
+    echo $objEmpleado->getDatosPersonales();
+    echo "Puesto: " . $objEmpleado->getPuesto();
+    echo "<br>" . $objEmpleado->getMensaje();
+
+    echo "<br>";
+    echo "<br>";
+
+    // $objCliente = new Cliente(55555, "Cliente No 2", 2);
+    // $objCliente->setCredito(300);
+    // $objCliente->setMensaje("Bienvenido cliente ");
+
+    
+    // echo $objCliente->getDatosPersonales();
+    // echo "Credito: " . $objCliente->getCredito();
+    // echo "<br>" . $objCliente->getMensaje();
+
+?>
